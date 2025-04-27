79fa40b579f4777c8f2e690ce8a9cf51d1164b70
coderzparadise <coderzparadise@gmail.com>
2025-04-27 09:58:06 -0500

    updated format

--- Leetcode/practice/day1.py
+++ Leetcode/practice/day1.py
@@ -4,6 +4,6 @@ Created on Sat Sep 27 09:33:40 2021
 
 @author: coderzparadise
 
-Fastest time to implement code and explain Data Structure: Graph
+Fastest time to implement code and explain:
     -X minutes
 """
